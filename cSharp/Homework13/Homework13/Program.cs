aa89ceda0e60c8edc83e244fe008b6f1514b65d0
Cyberia <seishiro631@gmail.com>
2026-06-01 15:08:57 +0300

    Add files via upload

---
+++ cSharp/Homework13/Homework13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string originalText = "Вот дом, Который построил Джек. А это пшеница, " +
+                                  "Которая в темном чулане хранится В доме, Который построил Джек. " +
+                                  "А это веселая птица-синица, Которая часто ворует пшеницу, " +
+                                  "которая в темном чулане хранится в доме Который построил Джек.";
+
+            string lowerCaseText = originalText.ToLower();
+
+            //разделители: пробел, знаки препинания
+            char[] delimiters = { ' ', ',', '.', '!', '?', ';', ':', '\n', '\r' };
+            string[] wordsArray = lowerCaseText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            // подсчёт частоты слов с помощью Dictionary
+            Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+
+            foreach (string currentWord in wordsArray)
+            {
+                if (wordFrequency.ContainsKey(currentWord))
+                    wordFrequency[currentWord]++;
+                else
+                    wordFrequency[currentWord] = 1;
+            }
+
+            Console.WriteLine("слов в тексте:\n");
+            Console.WriteLine("Слово\t\t\tКоличество");
+
+            foreach (KeyValuePair<string, int> entry in wordFrequency)
+            {
+                Console.WriteLine($"{entry.Key,-20}\t{entry.Value}");
+            }
+
+            Console.WriteLine($"\nВсего слов: {wordsArray.Length}, из них уникальных: {wordFrequency.Count}");
+        }
+    }
+}
---
+++ cSharp/Homework13/Homework13/Program2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Homework13
+{
+    internal class Program2
+    {
+        static void Main()
+        {
+            Person person = new Person();
+            person.PropertyChanged += (s, e) => Console.WriteLine($"Свойство '{e.PropertyName}' изменено.");
+
+            person.Name = "Мария";
+            person.Age = 30;
+            person.Age = 30;   
+            person.Name = "Анна";
+        }
+    }
+
+    //аргументы события
+    public class PropertyEventArgs : EventArgs
+    {
+        public string PropertyName { get; }
+        public PropertyEventArgs(string name) => PropertyName = name;
+    }
+
+    //делегат
+    public delegate void PropertyEventHandler(object sender, PropertyEventArgs e);
+
+    //интерфейс
+    interface IPropertyChanged
+    {
+        event PropertyEventHandler PropertyChanged;
+    }
+
+    //класс с уведомлениями об изменениях
+    public class Person : IPropertyChanged
+    {
+        private string name;
+        private int age;
+
+        public event PropertyEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                PropertyChanged?.Invoke(this, new PropertyEventArgs(nameof(Name)));
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+                PropertyChanged?.Invoke(this, new PropertyEventArgs(nameof(Age)));
+            }
+        }
+    }
+}
